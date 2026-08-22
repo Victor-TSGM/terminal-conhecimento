3621185c7921520e8fac817dda163845713e7fcc
Al3m40 <victor.martins00@outlook.com>
2026-08-22 15:06:03 -0300

    fixed errors

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Victor-TSGM/terminal-conhecimento.git', // INSIRA O NOME DO SEU REPOSITÓRIO AQUI
+  base: '/terminal-conhecimento/', // INSIRA O NOME DO SEU REPOSITÓRIO AQUI
 })
